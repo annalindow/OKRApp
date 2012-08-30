ddb54779155f647c3c4237c6c1a2e8f2598513d5
annalindow <anna.lindow@gmail.com>
2012-08-30 19:36:11 -0400

    fix controller for teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
 before_filter :authenticate_user! 
 
   def index
-   @users = current_user.team.users
+   @users = current_user.team.users.all
 
     respond_to do |format|
       format.html # index.html.erb
